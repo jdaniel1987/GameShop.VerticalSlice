7ef23897491d5639b11ae1baafc5a361bc5000f5
Jaime Daniel Delgado Ortega <jdaniel1987@hotmail.com>
2024-04-30 14:49:37 +0200

    delete scripts

--- dbo.Games.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] VARCHAR(MAX) NOT NULL, 
-    [Publisher] NCHAR(10) NOT NULL, 
-    [Price] DECIMAL(18, 2) NOT NULL
-)
